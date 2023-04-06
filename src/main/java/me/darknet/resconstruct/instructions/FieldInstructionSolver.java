fc06f705ea7fbad68110925b5cf40157320b80bc
Justus Garbe <gihihoh@gmail.com>
2023-04-06 20:20:17 +0200

    Add support for `getfield`

--- src/main/java/me/darknet/resconstruct/instructions/FieldInstructionSolver.java
+++ src/main/java/me/darknet/resconstruct/instructions/FieldInstructionSolver.java
@@ -12,10 +12,12 @@ public class FieldInstructionSolver implements InstructionSolver<FieldInsnNode> 
 	@Override
 	public void solve(FieldInsnNode instruction, SimFrame frame, ClassHierarchy hierarchy) {
 		int opcode = instruction.getOpcode();
+		PhantomClass owner = hierarchy.getOrCreate(Type.getObjectType(instruction.owner));
+		if(opcode == PUTFIELD || opcode == GETFIELD) {
+			owner.setIsObject(true);
+		}
 		if(opcode == PUTFIELD || opcode == PUTSTATIC) {
 			AbstractValue stackValue = frame.getStack(frame.getStackSize() - 1); // get stack value
-			PhantomClass owner = hierarchy.getOrCreate(Type.getObjectType(instruction.owner));
-			if(opcode == PUTFIELD) owner.setIsObject(true);
 			owner.addFieldUsage(opcode, instruction.name, stackValue.getType().getDescriptor());
 		}
 	}
